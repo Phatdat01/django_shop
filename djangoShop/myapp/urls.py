8df3d308a54c1236ba3c90f59ae0ee55d45bc464
Phatdat01 <phatdatlht2001@gmail.com>
2025-04-07 14:52:14 +0700

    feat: add form

--- djangoShop/myapp/urls.py
+++ djangoShop/myapp/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 #Define a list url patterns
 urlpatterns = [
-    path('', views.index, name="index"),
+    # path('', views.index, name="index"),
+    path('', views.home_view, name="home"),
+    path('contact/', views.contact_view, name="contact"),
+    path('contact/success', views.contact_success_view, name="contact-success"),
 ]
--- djangoShop/myapp/views.py
+++ djangoShop/myapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Tour
 from django.http import HttpResponse
+from .form import ContactForm
 
 # Create your views here.
 
@@ -11,3 +12,20 @@ def index(request):
     tours = Tour.objects.all()
     context = {'tours':tours}
     return render(request, 'tours/index.html', context)
+
+def home_view(request):
+    return render(request, 'myapp/home.html')
+
+def contact_view(request):
+    if request.method =="POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.send_email()
+            return redirect('contact-success')
+    else:
+        form = ContactForm()
+    context = {'form':form}
+    return render(request, 'myapp/contact.html', context)
+
+def contact_success_view(request):
+    return render(request, 'myapp/contact_success.html')
