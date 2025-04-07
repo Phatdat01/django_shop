8bbc1af4e68928c89d64243c3446db80faf83064
Phatdat01 <phatdatlht2001@gmail.com>
2025-04-07 17:27:49 +0700

    feat: add register, login, logout

--- djangoShop/djangoShop/settings.py
+++ djangoShop/djangoShop/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp.apps.MyappConfig',
-    # 'myapp',
+    # 'myapp.apps.MyappConfig',
+    'myapp',
 ]
 
 MIDDLEWARE = [
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
--- djangoShop/myapp/urls.py
+++ djangoShop/myapp/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 #Define a list url patterns
 urlpatterns = [
-    # path('', views.index, name="index"),
-    path('', views.home_view, name="home"),
+    path('', views.index, name="index"),
+    path('home/', views.home_view, name="home"),
     path('contact/', views.contact_view, name="contact"),
     path('contact/success', views.contact_success_view, name="contact-success"),
 ]
